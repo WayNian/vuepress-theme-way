0bcd4424ffa26ca2128dcce262682247ada0dae8
waynian <waynian@foxmail.com>
2022-06-15 22:04:54 +0800

    feat: 增加基础页面

---
+++ packages/theme/src/client/components/Common.vue
@@ -0,0 +1,22 @@
+<template>
+  <Navbar></Navbar>
+  <div class="theme-way-layout">
+    <aside class="left-view">
+      <PersonView></PersonView>
+    </aside>
+    <main class="main-view">
+      <div class="main-layout">
+        5615615
+        <slot></slot>
+      </div>
+    </main>
+    <aside class="right-view">gwagwag</aside>
+  </div>
+</template>
+
+<script setup lang="ts">
+import Navbar from '@theme/Navbar.vue'
+import PersonView from '@theme/PersonView.vue'
+</script>
+
+<style scoped></style>
