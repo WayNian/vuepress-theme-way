3d2f84070b6c2feb2aaa46ba90c950f2c5d43dd3
waynian <waynian@foxmail.com>
2022-06-16 22:09:25 +0800

    style: 优化navbar的显示

--- packages/theme/src/client/components/Navbar.vue
+++ packages/theme/src/client/components/Navbar.vue
@@ -1,17 +1,21 @@
 <template>
   <nav class="navbar">
-    <RouterLink to="/">
-      <div class="logo">
-        <img src="https://pic.imgdb.cn/item/5d804d9d451253d1783c4c25.png" />
-        <span>这是文字</span>
+    <div class="navbar-layout">
+      <div class="flex">
+        <RouterLink to="/">
+          <div class="logo">
+            <img src="https://pic.imgdb.cn/item/5d804d9d451253d1783c4c25.png" />
+            <span>这是文字</span>
+          </div>
+        </RouterLink>
+
+        <ul class="navbar-menu">
+          <li v-for="item in navLinks" :key="item.text">{{ item.text }}</li>
+        </ul>
       </div>
-    </RouterLink>
 
-    <ul class="navbar-menu">
-      <li v-for="item in navLinks" :key="item.text">{{ item.text }}</li>
-    </ul>
-
-    <button @click="changeMode">切换主题</button>
+      <button @click="changeMode">切换主题</button>
+    </div>
   </nav>
 </template>
 
