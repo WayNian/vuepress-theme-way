88d5f429b6baf943c28c9379f793e516c25860d3
waynian <waynian@foxmail.com>
2022-06-23 16:19:15 +0800

    feat: 添加waline

--- packages/plugins/plugin-way-waline/src/client/components/Waline.vue
+++ packages/plugins/plugin-way-waline/src/client/components/Waline.vue
@@ -1,7 +1,17 @@
 <template>
-  <div style="font-size: 50px; color: red">这是评论区</div>
+  <div id="waline"></div>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { init } from '@waline/client'
+import { onMounted } from 'vue'
+import '@waline/client/dist/waline.css'
+onMounted(() => {
+  init({
+    el: '#waline',
+    serverURL: 'https://theme-way-waline-2e8axyq7o-waynian.vercel.app',
+  })
+})
+</script>
 
 <style scoped></style>
