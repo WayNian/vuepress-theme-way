efacac976311c5e8f9e497fc40b4c0552fca664f
waynian <waynian@foxmail.com>
2022-06-30 09:44:42 +0800

    feat: x修改滚动行为

--- packages/theme/src/client/config.ts
+++ packages/theme/src/client/config.ts
@@ -5,10 +5,19 @@ import {
   setupBlogPages,
   setupColorMode,
   setupSidebarItems,
+  useScrollPromise,
 } from './composables'
 import { setupTags } from './composables/useTag'
 
 export default defineClientConfig({
+  enhance({ app, router }) {
+    // handle scrollBehavior with transition
+    const scrollBehavior = router.options.scrollBehavior!
+    router.options.scrollBehavior = async (...args) => {
+      await useScrollPromise().wait()
+      return scrollBehavior(...args)
+    }
+  },
   setup() {
     setupColorMode()
     setupSidebarItems()
