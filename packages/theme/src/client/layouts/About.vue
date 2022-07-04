229cecf19224851ba2b53b92a874ecb8b532962f
waynian <waynian@foxmail.com>
2022-07-04 15:20:34 +0800

    docs: 更新文档

--- packages/theme/src/client/layouts/About.vue
+++ packages/theme/src/client/layouts/About.vue
@@ -1,6 +1,10 @@
 <template>
   <Common>
-    <div>这是关于页</div>
+    <img
+      align="right"
+      src="https://github-readme-stats.vercel.app/api?username=waynian&show_icons=true&icon_color=0366d6&bg_color=ffffff&hide_title=true&hide=contribs&include_all_commits=true"
+      alt="waynian's github stats"
+    />
   </Common>
 </template>
 
