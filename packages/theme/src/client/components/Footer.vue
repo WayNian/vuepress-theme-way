d119cc11416940bf5d969f416136c9c89ad6742f
waynian <waynian@foxmail.com>
2022-07-06 10:44:31 +0800

    feat: 增加底部跳转链接

--- packages/theme/src/client/components/Footer.vue
+++ packages/theme/src/client/components/Footer.vue
@@ -1,16 +1,27 @@
 <template>
   <div class="footer">
-    <p>版权所有 © 2022-2022 Waynian</p>
+    <p>版权所有 © 2022-2022 {{ personInfo.name }}</p>
     <p>
       Theme
       <a href="https://github.com/WayNian/vuepress-theme-way" target="_blank"
         >Way</a
       >
-      By Waynian | All Rights Reserved
+      By
+      <a href="https://waynian.com" target="_blank">waynian</a>
+      | All Rights Reserved
     </p>
   </div>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { computed } from 'vue'
+import type { PersonInfo } from '../../shared'
+import { useThemeLocaleData } from '../composables'
+
+const themeLocale = useThemeLocaleData()
+const personInfo = computed(() => {
+  return themeLocale.value.personInfo as PersonInfo
+})
+</script>
 
 <style scoped></style>
