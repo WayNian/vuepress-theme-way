fec9247868343700d00a209d9aa6cb3d49b06e02
waynian <waynian@foxmail.com>
2022-07-11 16:53:28 +0800

    feat: 增加目录

--- packages/theme/src/client/components/Catalog.vue
+++ packages/theme/src/client/components/Catalog.vue
@@ -1,11 +1,19 @@
 <template>
-  <Card v-if="page.headers.length" class="person-view"> 文章目录 </Card>
+  <Card v-if="page.headers.length" class="catalog">
+    <TitleItem :title-list="page.headers"> </TitleItem>
+  </Card>
 </template>
 
 <script setup lang="ts">
 import Card from '@theme/Card.vue'
+import TitleItem from '@theme/TitleItem.vue'
 import { usePageData } from '@vuepress/client'
 const page = usePageData()
+console.log('🚀 ~ file: Catalog.vue ~ line 9 ~ page', page.value.headers)
 </script>
 
-<style scoped></style>
+<style scoped>
+.catalog {
+  height: min-content;
+}
+</style>
---
+++ packages/theme/src/client/components/TitleItem.vue
@@ -0,0 +1,27 @@
+<template>
+  <ul>
+    <li v-for="item in props.titleList" :key="item.slug">
+      <a :href="`#${item.slug}`">{{ item.title }} - {{ item.level }}</a>
+      <TitleItem :title-list="item.children"> </TitleItem>
+    </li>
+  </ul>
+</template>
+
+<script setup lang="ts">
+import TitleItem from '@theme/TitleItem.vue'
+import type { PageHeader } from '@vuepress/shared'
+import type { PropType } from 'vue'
+
+const props = defineProps({
+  titleList: {
+    type: Array as PropType<PageHeader[]>,
+    required: true,
+  },
+})
+</script>
+
+<style scoped>
+ul {
+  margin: 0;
+}
+</style>
