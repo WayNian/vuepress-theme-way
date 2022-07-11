6c50d02b1d58be50d2853ae33c60ddb8ec47ce43
waynian <waynian@foxmail.com>
2022-07-11 17:09:11 +0800

    feat: 增加目录点击跳转

--- packages/theme/src/client/components/TitleItem.vue
+++ packages/theme/src/client/components/TitleItem.vue
@@ -1,8 +1,15 @@
 <template>
   <ul>
-    <li v-for="item in props.titleList" :key="item.slug">
-      <a :href="`#${item.slug}`">{{ item.title }} - {{ item.level }}</a>
-      <TitleItem :title-list="item.children"> </TitleItem>
+    <li v-for="(item, index) in props.titleList" :key="item.slug">
+      <a :href="`#${item.slug}`" :class="`catalog-title-${item.level}`">{{
+        item.title
+      }}</a>
+      <TitleItem
+        :title-list="item.children"
+        class="sub-title"
+        :title-num="index + 1"
+      >
+      </TitleItem>
     </li>
   </ul>
 </template>
@@ -17,11 +24,26 @@ const props = defineProps({
     type: Array as PropType<PageHeader[]>,
     required: true,
   },
+  titleNum: {
+    type: Number,
+    default: null,
+  },
 })
 </script>
 
 <style scoped>
 ul {
   margin: 0;
+}
+.catalog-title-1 {
+  font-size: 1.5rem;
+  font-weight: bold;
+}
+.catalog-title-2 {
+  font-size: 1rem;
+  font-weight: bold;
+}
+.sub-title {
+  padding-left: 10px;
 }
 </style>
