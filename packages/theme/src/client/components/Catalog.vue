0df3b073e88a5f572cfa345d6b559ef838c588be
waynian <waynian@foxmail.com>
2022-07-12 17:29:09 +0800

    feat: 滚动切换锚点

--- packages/theme/src/client/components/Catalog.vue
+++ packages/theme/src/client/components/Catalog.vue
@@ -1,7 +1,7 @@
 <template>
   <Card v-if="page.headers.length" class="catalog">
     <span>目录</span>
-    <TitleItem :title-list="page.headers" :active-anchor="route.hash">
+    <TitleItem :title-list="page.headers" :active-anchor="activeAnchor">
     </TitleItem>
   </Card>
 </template>
@@ -9,11 +9,46 @@
 <script setup lang="ts">
 import Card from '@theme/Card.vue'
 import TitleItem from '@theme/TitleItem.vue'
+import type { PageHeader } from '@vuepress/client'
 import { usePageData } from '@vuepress/client'
-import { onMounted } from 'vue'
+import { onBeforeUnmount, onMounted, ref } from 'vue'
 import { useRoute } from 'vue-router'
+
 const page = usePageData()
 const route = useRoute()
+const activeAnchor = ref('#' + route.hash)
+const headers = ref<PageHeader[]>([])
+
+const flatHeaders = (headers: PageHeader[]): [] => {
+  return headers.reduce((acc, header) => {
+    acc.push(header as never)
+    if (header.children) {
+      acc.push(...flatHeaders(header.children))
+    }
+    return acc
+  }, [])
+}
+onMounted(() => {
+  headers.value = flatHeaders(page.value.headers)
+})
+const handleScroll = (): void => {
+  headers.value.forEach((item) => {
+    const anchor = document.querySelector(`#${item.slug}`)
+    if (anchor) {
+      const anchorTop = anchor.getBoundingClientRect().top
+      if (anchorTop < 0) {
+        activeAnchor.value = `#${item.slug}`
+      }
+    }
+  })
+}
+
+onMounted(() => {
+  document.addEventListener('scroll', handleScroll)
+})
+onBeforeUnmount(() => {
+  document.removeEventListener('scroll', handleScroll)
+})
 </script>
 
 <style scoped>
